f237150fb2ed6f6b310800012ae73656edc82cc2
Lucas <lucasgdeogomes@gmail.com>
2026-08-11 21:52:51 -0400

    s

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import "fmt"
+
+var status = false
+func init() {
+	fmt.Println("Started...")
+}
+func main() {
+	fmt.Println("Ok")
+	status = true
+	if status {
+		fmt.Println("Status: Online")
+	}
+}
+
