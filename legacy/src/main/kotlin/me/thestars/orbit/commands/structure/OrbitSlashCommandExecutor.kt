5b31f2d8563375b91713a6ba5588d5264abc719d
Lucas <lucasgdeogomes@gmail.com>
2026-08-11 21:44:49 -0400

    s

--- legacy/src/main/kotlin/me/thestars/orbit/commands/structure/OrbitSlashCommandExecutor.kt
+++
@@ -1,7 +0,0 @@
-package me.thestars.orbit.commands.structure
-
-import me.thestars.orbit.commands.UnleashedCommandContext
-
-abstract class OrbitSlashCommandExecutor {
-    abstract suspend fun execute(context: UnleashedCommandContext)
-}
--- legacy/src/main/kotlin/me/thestars/orbit/database/data/InnerMemberResponse.kt
+++
@@ -1,6 +0,0 @@
-package me.thestars.orbit.database.data
-
-import  kotlinx.serialization.Serializable
-
-@Serializable
-data class InnerMemberResponse(val id: Long, var currency: Long)
